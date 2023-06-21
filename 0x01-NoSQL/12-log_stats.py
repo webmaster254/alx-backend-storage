35cdd4fe8ddc6d633c16491a090b920500ec0422
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2023-06-21 17:53:14 +0300

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
-"""an improved script that provides some stats about Nginx logs"""
+"""a Python script that provides some stats about Nginx logs"""
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    '''
-    provides some stats about Nginx logs
-    improved by adding the top 10 of the most present IPs
-    '''
+    '''provides some stats about Nginx logs'''
     client = MongoClient('mongodb://127.0.0.1:27017')
-    col = client['logs']['nginx']
+    col = client.logs.nginx
     print("{} logs".format(col.estimated_document_count()))
     print("Methods:")
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
@@ -17,21 +14,3 @@ if __name__ == "__main__":
         print("\tmethod {}: {}".format(method, count))
     status_get = col.count_documents({'method': 'GET', 'path': "/status"})
     print("{} status check".format(status_get))
-    print("IPs:")
-    topIps = col.aggregate([
-        {"$group":
-            {
-                "_id": "$ip",
-                "count": {"$sum": 1}
-            }
-        },
-        {"$sort": {"count": -1}},
-        {"$limit": 10},
-        {"$project": {
-            "_id": 0,
-            "ip": "$_id",
-            "count": 1
-        }}
-    ])
-    for ip in topIps:
-        print("\t{}: {}".format(ip.get('ip'), ip.get('count')))
